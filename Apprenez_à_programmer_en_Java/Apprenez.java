c8a6ea39f908d71f225dca9e36ea3cd2c9ad0655
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-07-06 18:38:49 +0000

    Apprendre toutes les bases

---
+++ Apprenez_à_programmer_en_Java/Apprenez.java
@@ -0,0 +1,12 @@
+public class Apprenez {
+
+    public static void main(String[] args) {
+        
+        int incomes = 500;
+        int saving = 1000;
+        int x = incomes + saving;
+
+        System.out.println(x);
+    }
+
+}
